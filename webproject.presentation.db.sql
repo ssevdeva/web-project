a6001cfe32c7f72ae3c4c16757eda72dd53cb75f
Sanya Sarieva <s.g.sevdeva@gmail.com>
2023-06-24 16:13:44 +0300

    Fixed the SQL script for DB creation so that the default entries correspond to the entries added by the server

--- webproject.presentation.db.sql
+++ webproject.presentation.db.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS presentations (
 
 -- Insert the presentations
 INSERT INTO presentations (topic, tag, content) VALUES
-  ('PHP and HTML', 'PHP, HTML', '<h1>PHP and HTML Presentation</h1><p>This presentation covers PHP and HTML integration.</p>'),
-  ('PHP, SQL & HTML', 'PHP, SQL, HTML', '<h1>PHP, SQL & HTML Presentation</h1><p>This presentation covers PHP, SQL, and HTML integration.</p>'),
-  ('Forms and Regex', 'Forms, Regex', '<h1>Forms and Regex Presentation</h1><p>This presentation covers forms and regular expressions.</p>'),
-  ('PHP', 'PHP', '<h1>PHP Presentation</h1><p>This presentation focuses on PHP programming language.</p>');
+  ('PHP and HTML', 'PHP, HTML', 'a:1:{i:0;s:91:"<h1>PHP and HTML Presentation</h1><p>This presentation covers PHP and HTML integration.</p>";}'),
+  ('PHP, SQL & HTML', 'PHP, SQL, HTML', 'a:1:{i:0;s:104:"<h1>PHP, SQL &amp; HTML Presentation</h1><p>This presentation covers PHP, SQL, and HTML integration.</p>";}'),
+  ('Forms and Regex', 'Forms, Regex', 'a:1:{i:0;s:99:"<h1>Forms and Regex Presentation</h1><p>This presentation covers forms and regular expressions.</p>";}'),
+  ('PHP', 'PHP', 'a:1:{i:0;s:86:"<h1>PHP Presentation</h1><p>This presentation focuses on PHP programming language.</p>";}');
