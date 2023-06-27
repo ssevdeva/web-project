1a44b165591180a523da4166efacc1b137e42c5e
Sanya Sarieva <s.g.sevdeva@gmail.com>
2023-06-27 12:32:55 +0300

    Enhance presentations creation

--- php/download.php
+++ php/download.php
@@ -5,72 +5,121 @@ ini_set('display_errors', 1);
 require_once 'db-config.php';
 
 function buildPresentation($slides, $title) {
-    $html = '<html>';
-    $html .= '<head>';
-    $html .= '<meta content="width=device-width, initial-scale=1" name="viewport" charset="UTF-8">';
-    $html .= '<title>' . $title . '</title>';
-    $html = '<div class="slideshow">';
-    
+    $dom = new DOMDocument('1.0', 'UTF-8');
+    $dom->formatOutput = true;
+
+    $html = $dom->createElement('html');
+    $dom->appendChild($html);
+
+    $head = $dom->createElement('head');
+    $html->appendChild($head);
+
+    $meta = $dom->createElement('meta');
+    $meta->setAttribute('content', 'width=device-width, initial-scale=1');
+    $meta->setAttribute('name', 'viewport');
+    $meta->setAttribute('charset', 'UTF-8');
+    $head->appendChild($meta);
+
+    $titleElement = $dom->createElement('title', $title);
+    $head->appendChild($titleElement);
+
+    $slideshow = $dom->createElement('div');
+    $slideshow->setAttribute('class', 'slideshow');
+    $html->appendChild($slideshow);
+
     // Navigation
-    $html .= '<div class="slideshow-navigation">';
-    $html .= '<button class="prev-slide">Prev</button>';
-    $html .= '<button class="next-slide">Next</button>';
-    $html .= '</div>';
-    
+    $navigation = $dom->createElement('div');
+    $navigation->setAttribute('class', 'slideshow-navigation');
+    $slideshow->appendChild($navigation);
+
+    $prevButton = $dom->createElement('button', 'Prev');
+    $prevButton->setAttribute('class', 'prev-slide');
+    $navigation->appendChild($prevButton);
+
+    $nextButton = $dom->createElement('button', 'Next');
+    $nextButton->setAttribute('class', 'next-slide');
+    $navigation->appendChild($nextButton);
+
     // Contents sidebar
-    $html .= '<div class="slideshow-contents">';
-    $html .= '<ul>';
-    
+    $contents = $dom->createElement('div');
+    $contents->setAttribute('class', 'slideshow-contents');
+    $slideshow->appendChild($contents);
+
+    $ul = $dom->createElement('ul');
+    $contents->appendChild($ul);
+
     foreach ($slides as $index => $slide) {
-        $html .= '<li><a href="#slide-' . ($index + 1) . '">Slide ' . ($index + 1) . '</a></li>';
+        // Find the first <h1> tag in the slide
+        preg_match('/<h1>(.*?)<\/h1>/i', $slide, $matches);
+        $slideName = isset($matches[1]) ? $matches[1] : 'Slide ' . ($index + 1);
+
+        $li = $dom->createElement('li');
+        $ul->appendChild($li);
+
+        $a = $dom->createElement('a', $slideName);
+        $a->setAttribute('href', '#slide-' . ($index + 1));
+        $li->appendChild($a);
     }
-    
-    $html .= '</ul>';
-    $html .= '</div>';
-    
+
     // Slides
     foreach ($slides as $index => $slide) {
-        $html .= '<div id="slide-' . ($index + 1) . '" class="slide">';
-        $html .= $slide;
-        $html .= '</div>';
+        $slideDiv = $dom->createElement('div');
+        $slideDiv->setAttribute('id', 'slide-' . ($index + 1));
+        $slideDiv->setAttribute('class', 'slide');
+        $slideshow->appendChild($slideDiv);
+
+        $slideDiv->appendChild($dom->createCDATASection($slide));
     }
-    
-    $html .= '</div>';
-    
+
     // JavaScript for slideshow functionality
-    $html .= '<script>';
-    $html .= 'var currentSlide = 1;';
-    $html .= 'var totalSlides = ' . count($slides) . ';';
-    $html .= 'document.addEventListener("DOMContentLoaded", function() {';
-    $html .= '  showSlide(currentSlide);';
-    $html .= '  document.querySelector(".prev-slide").addEventListener("click", function() {';
-    $html .= '    navigateSlide(-1);';
-    $html .= '  });';
-    $html .= '  document.querySelector(".next-slide").addEventListener("click", function() {';
-    $html .= '    navigateSlide(1);';
-    $html .= '  });';
-    $html .= '});';
-    $html .= 'function showSlide(n) {';
-    $html .= '  var slides = document.getElementsByClassName("slide");';
-    $html .= '  var contentsLinks = document.getElementsByTagName("a");';
-    $html .= '  if (n > slides.length) { currentSlide = 1; }';
-    $html .= '  if (n < 1) { currentSlide = slides.length; }';
-    $html .= '  for (var i = 0; i < slides.length; i++) {';
-    $html .= '    slides[i].style.display = "none";';
-    $html .= '  }';
-    $html .= '  for (var i = 0; i < contentsLinks.length; i++) {';
-    $html .= '    contentsLinks[i].classList.remove("active");';
-    $html .= '  }';
-    $html .= '  slides[currentSlide - 1].style.display = "block";';
-    $html .= '  contentsLinks[currentSlide - 1].classList.add("active");';
-    $html .= '}';
-    $html .= 'function navigateSlide(n) {';
-    $html .= '  showSlide(currentSlide += n);';
-    $html .= '}';
-    $html .= '</script>';
-    
-    return $html;
+    $script = $dom->createElement('script');
+    $script->appendChild($dom->createTextNode('
+        var currentSlide = 1;
+        var totalSlides = ' . count($slides) . ';
+        document.addEventListener("DOMContentLoaded", function() {
+            showSlide(currentSlide);
+            document.querySelector(".prev-slide").addEventListener("click", function() {
+                navigateSlide(-1);
+            });
+            document.querySelector(".next-slide").addEventListener("click", function() {
+                navigateSlide(1);
+            });
+
+            var links = document.querySelectorAll(".slideshow-contents a");
+            links.forEach(function(link) {
+                link.addEventListener("click", function(event) {
+                    event.preventDefault();
+                    var slideId = this.getAttribute("href").substring(1);
+                    showSlide(parseInt(slideId.replace("slide-", "")));
+                });
+            });
+        });
+        function showSlide(n) {
+            var slides = document.getElementsByClassName("slide");
+            var contentsLinks = document.querySelectorAll(".slideshow-contents a");
+            if (n > slides.length) { n = 1; }
+            if (n < 1) { n = slides.length; }
+            for (var i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (var i = 0; i < contentsLinks.length; i++) {
+                contentsLinks[i].classList.remove("active");
+            }
+            slides[n - 1].style.display = "block";
+            contentsLinks[n - 1].classList.add("active");
+            currentSlide = n;
+        }
+        function navigateSlide(n) {
+            showSlide(currentSlide += n);
+        }
+    '));
+    $slideshow->appendChild($script);
+
+    $htmlContent = $dom->saveHTML();
+    return $htmlContent;
 }
+
+
 
 // Check if the presentation ID is provided
 if (!isset($_GET['id']) || empty($_GET['id'])) {
