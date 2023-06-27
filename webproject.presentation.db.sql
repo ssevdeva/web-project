711234fc8a87137d3c99c101201a0e7daa069139
Sanya Sarieva <s.g.sevdeva@gmail.com>
2023-06-27 11:14:31 +0300

    Revert a change in the SQL script for DB creation

--- webproject.presentation.db.sql
+++ webproject.presentation.db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS Presentations CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS Presentations;
 
 -- Create user and grant privileges
 CREATE USER 'webadmin'@'localhost' IDENTIFIED BY 'webadminpass';
